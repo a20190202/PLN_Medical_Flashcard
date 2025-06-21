d3a92e0760fb2904bde354e264b0afd007954875
alez-2210 <a20201403@pucp.edu.pe>
2025-06-20 23:44:52 -0500

    pequeño ajuste front

--- apps/Frontend/app/layout.tsx
+++ apps/Frontend/app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Asap, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const asap = Asap({
+  variable: "--font-asap",
   subsets: ["latin"],
+  display: "swap",
+  axes: ["wdth"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,10 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={`${asap.variable} ${geistMono.variable} antialiased`}>
+      <body>
         {children}
       </body>
     </html>
